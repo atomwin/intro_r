95b57b4060c9310126bf12a268fdde2be0cfef5f
Adam N. -laptop <adam.nguyen@phs.hctx.net>
2024-09-06 13:28:28 -0500

    datetypes

--- code/training_script.R
+++ code/training_script.R
@@ -1,1 +1,20 @@
-print("hello world")
+sum(1, 2)
+
+df <- read.csv("C:/Users/adam.nguyen/Downloads/example_data.csv")
+df
+
+summary(df$Race)
+
+getwd()
+
+df_2 <- read.csv(paste0(getwd(),"/data/example_data.csv"))
+setwd()
+
+paste0(getwd(),"/data/example_data.csv")
+
+# install.packages("tidyverse")
+
+library(tidyverse)
+
+glimpse(df)
+
