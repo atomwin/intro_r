6266540448c8c73694439b3495ec7680a0bf2e91
Nguyen <Adam.Nguyen@phs.hctx.net>
2024-09-19 16:26:07 -0500

    update learning sesion

--- sessions/session_3/session_3_code.R
+++ sessions/session_3/session_3_code.R
@@ -24,9 +24,6 @@ glimpse(df_csv) # tidyverse function
 
 str(df_csv) # base r function 
 
-
-
-
 # base r vs libraries: creator vs contributors: inventors vs improvements
 
 # what are objects/data structures? for now we focus data frames (tables)
@@ -50,7 +47,6 @@ colnames(df_csv_clean_names)
 df_csv_clean_names <- clean_names(df_csv, case = "screaming_snake") # no pipe operator
 
 df_csv_clean_names <- clean_names(df_csv) # no pipe operator, default
-
 
 # df_csv_clean_names <- df_csv %>% clean_names(case = "screaming_snake") # show how to read documentation, why and how ?
 # # clean_names() is like a tradional hammer, clean_names(case = "screaming_snake") is a modified hammer ie slege hammer
@@ -87,15 +83,7 @@ df_csv_formating <- df_csv %>%
 # clean col names---------------------------------------------------------------
 
 # FINAL CODE--------------------------------------------------------------------
-df_csv <- read.csv("C:/GitHub Repo/Adams_Devops/ri_training/draft_wk/example_data.csv")  
-
-df_csv_formating <- df_csv %>% 
-  clean_names() %>% 
-  rename("ethnicity" = "hispanic", "exercise" = "excerise") %>% 
-  select(dob, ethnicity, race, sex, zip_code, insurance, exercise, max_squat)
-
-# or u can write it like this:
-df_csv <- read.csv("C:/GitHub Repo/Adams_Devops/ri_training/draft_wk/example_data.csv")  
+df_csv <- read.csv(paste0(getwd(), "/data/example_data.csv")) 
 
 df_csv_clean_names <- df_csv %>% clean_names()
 
@@ -103,9 +91,14 @@ df_csv_new_column_names <- df_csv_clean_names %>%
   rename("ethnicity" = "hispanic", "exercise" = "excerise")
 
 df_csv_select <- df_csv_new_column_names %>% 
-  select(dob, ethnicity, race, sex, zip_code, insurance, exercise, max_squat)
+  select(dob, ethnicity, race, sex, zip_code, insurance, exercise, everything())
 
 df_csv_formating <- df_csv_select
+
+# df_csv_formating <- df_csv %>% 
+#   clean_names() %>% 
+#   rename("ethnicity" = "hispanic", "exercise" = "excerise") %>% 
+#   select(dob, ethnicity, race, sex, zip_code, insurance, exercise, max_squat, everything())
 
 # FINAL CODE--------------------------------------------------------------------
 ################################################################################
@@ -122,7 +115,7 @@ df_csv_formating <- df_csv_select
 # use mutate, case_when to clean the race column
 unique(df_csv_formating$race)
 
-df_wk2_case_when <- df_csv_formating %>%
+df_csv_race_edit <- df_csv_formating %>%
   mutate(
     edit_race = case_when(
       race == "White" ~ "white",
@@ -132,7 +125,7 @@ df_wk2_case_when <- df_csv_formating %>%
       race == "BLACK" ~ "black",
       race == "Mixed" ~ "mixed",
       race == "Caucasian" ~ "white",
-      race == "CaucasiaN" ~ "White",
+      race == "CaucasiaN" ~ "white",
       race == "Af-Am" ~ "black",
       race == "Asian" ~ "asian",
       race == "ASiaN" ~ "asian",
@@ -141,69 +134,122 @@ df_wk2_case_when <- df_csv_formating %>%
   ) %>% 
   select(race, edit_race)
 
-unique(df_wk2_case_when[,c('race','edit_race')])
+# see the unique combinations to check work
+unique(df_csv_race_edit[,c('race','edit_race')])
 
-# why do more work? STANDARIZE!
-df_wk2_case_when_lower <- df_csv_formating %>%
+# why do more work? STANDARIZE!, show why we dont need white ~ white bc of TRUE
+df_csv_race_edit_lower <- df_csv_formating %>%
   mutate(
     lower_case_race = tolower(race),
     edit_race = case_when(
-      lower_case_race == "white" ~ "white",
-      lower_case_race == "other" ~ "other",
-      lower_case_race == "black" ~ "black",
-      lower_case_race == "mixed" ~ "mixed",
+      lower_case_race == "white" ~ "white", #
+      lower_case_race == "other" ~ "other", #
+      lower_case_race == "black" ~ "black", #
+      lower_case_race == "mixed" ~ "mixed", #
       lower_case_race == "caucasian" ~ "white",
-      lower_case_race == "af-am" ~ "black",
-      lower_case_race == "asian" ~ "asian",
-      TRUE ~ race
+      lower_case_race == "am-af" ~ "black",
+      lower_case_race == "asian" ~ "asian", #
+      TRUE ~ lower_case_race
     )
-  ) %>% 
-  select(race, lower_case_race, edit_race)
+  ) # %>% 
+  # select(race, lower_case_race, edit_race)
 
-# how can you test if correct? QA ie unit test?
-vec_race <- c("white","black", "asian", "mixed", "other") %>% sort() # 
+# unique(df_csv_race_edit_lower[,c('race','edit_race')])
+# 
+# # how can you test if correct? QA ie unit test?
+# # create vector of what we expect
+# vec_race <- c("white","black", "asian", "mixed", "other") %>% sort() 
+# 
+# # df to show combination, just like unique
+# qa_race_edit <- df_csv_race_edit_lower %>% 
+#   group_by(edit_race) %>% 
+#   reframe(og = unique(race)) %>% 
+#   arrange(edit_race)
+# 
+# if (identical(unique(qa_race_edit$edit_race), vec_race) == TRUE){
+#   insight::print_color("PASS: vectors match", "green")
+# } else {
+#   insight::print_color("!!FAIL: vectors NO match!!", "red")
+# }
 
-qa_race_edit <- df_wk2_case_when_lower %>% 
-  group_by(edit_race) %>% 
-  reframe(og = unique(race)) %>% 
-  arrange(edit_race)
-
-ifelse(unique(qa_race_edit$edit_race) == vec_race, print("pass"), print("FAIL"))
-
-unique(qa_race_edit$edit_race) == vec_race
-
-if (unique(qa_race_edit$edit_race)[i] %in% vec_race){
-  print("pass")
-} else {
-  print("FAIL")
-}
-
-any(unique(qa_race_edit$edit_race) = vec_race)
-  
-unique(df_wk2_case_when_lower[,c('race','edit_race')])
-
-unique(dfc(df_wk2_case_when_lower$race,df_wk2_case_when_lower$edit_race ))
-
-
-
+df_csv_race_clean <- df_csv_race_edit_lower %>% 
+  mutate(race = edit_race) %>% 
+  select(-c("lower_case_race", "edit_race"))
 # now in scenarios where there are a lot of unique values for race;
 # regular expression (gsub()) or
 # string mathing (lib(fuzzywuzzyR)) may help find the best replacement
 
 # calc mean.... do mean() and old school way ie sum/3 or sum/length(of unqiue col)
+df_average <- df_csv_race_clean %>% 
+  mutate(avg_strength_manual = (max_squat + max_bench + max_deadlift) / 3,
+         avg_strength_func = rowMeans(select(., max_squat,max_bench ,max_deadlift))) %>%  #  c(max_squat ,max_bench ,max_deadlift)
+select(-avg_strength_manual)
 
 # FINAL CODE--------------------------------------------------------------------
+df_clean_race_strength <- df_csv_formating %>%
+  mutate(
+    lower_case_race = tolower(race),
+    edit_race = case_when(
+      lower_case_race == "white" ~ "white", #
+      lower_case_race == "other" ~ "other", #
+      lower_case_race == "black" ~ "black", #
+      lower_case_race == "mixed" ~ "mixed", #
+      lower_case_race == "caucasian" ~ "white",
+      lower_case_race == "am-af" ~ "black",
+      lower_case_race == "asian" ~ "asian", #
+      TRUE ~ lower_case_race
+    ),
+    race = edit_race,
+    avg_strength = rowMeans(select(., max_squat,max_bench ,max_deadlift))
+  ) %>% 
+  select(-c("lower_case_race", "edit_race")) 
+
+# qa
+# unique(df_clean_race_strength[,c('race','edit_race')])
+# unique(df_clean_race_strength[,c('lower_case_race','edit_race')])
+
+vec_race <- c("white","black", "asian", "mixed", "other") %>% sort() 
+
+if (identical(sort(unique(df_clean_race_strength$race)), vec_race) == TRUE){
+  insight::print_color("PASS: vectors match", "green")
+} else {
+  insight::print_color("!!FAIL: vectors NO match!!", "red")
+}
+# FINAL CODE--------------------------------------------------------------------
+
 ################################################################################
 # Session 3: clean data WORK IN PROGRESSS#######################################
 ################################################################################
 
 
 ################################################################################
-# WK 3: datetime WORK IN PROGRESSS###############################################################
+# WK 3: datetime WORK IN PROGRESSS##############################################
 ################################################################################
 
 
 ################################################################################
-# WK 3: datetime WORK IN PROGRESSS###############################################################
+# WK 3: datetime WORK IN PROGRESSS##############################################
 ################################################################################
+
+################################################################################
+# WK 4: for logical value for x, create function using casewhen? refer to sex,
+# insurance col ################################################################
+
+glimpse(df_clean_race_strength)
+
+df_sex <- df_clean_race_strength %>%
+  mutate(new_sex = case_when(
+    grepl('%m', tolower(sex)) ~ "m",
+    grepl('%f', tolower(sex)) ~ "f"
+  ))
+
+df_sex
+################################################################################
+
+################################################################################
+# WK 4: for logical value for x, create function using casewhen? refer to sex,
+# insurance col ################################################################
+################################################################################
+
+
 
